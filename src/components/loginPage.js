731b7702837f4c571537c539f55e99b8496cd259
Putra Utama <putra.utamajaya@Hotmail.com>
2018-08-27 20:26:42 +0700

    update autentication failed

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -6,15 +6,27 @@ import { onLogin } from '../actionCreator';
 import '../supports/css/components/loginPage.css';
 
 class loginPage extends Component {
+
+    state = {
+        count: 0
+    }
+
     onLoginClick = () => {
         let email = this.refs.email.value;
         let password = this.refs.password.value;
 
         this.props.onLogin({ email, password});
+
+        this.setState(
+            {
+            count: this.state.count + 1
+            }
+        );
     }
 
     render() {
-        if(this.props.auth.username == ""){
+        console.log(this.state.count);
+        if(this.props.auth.username == "" && this.state.count == 0){
             return(
                 <div className="login-background">
                 <div className="container">
@@ -53,6 +65,49 @@ class loginPage extends Component {
                 </div>
                     );
                 }
+
+        else if(this.props.auth.username == "" && this.state.count != 0) {
+            return(
+                <div className="login-background">
+                <div className="container">
+                    <h1 className="form-heading">login Form</h1>
+    
+                    <div className="login-form">
+                        <div className="main-div">
+                            <div className="panel">
+                                <h2>Admin Login</h2>
+                                <p>Please enter your email and password</p>
+                            </div>
+    
+                        <form id="Login">
+    
+                            <div className="form-group">
+                                <input type="email" ref="email" className="form-control" id="inputEmail" placeholder="Email Address" />
+                            </div>
+    
+                            <div className="form-group">
+                                <input type="password" ref="password" className="form-control" id="inputPassword" placeholder="Password" />
+                            </div>
+
+                            <p style={{ color: "red" }}>Autentication Failed</p>
+
+                            <div className="forgot">
+                                <a href="reset.html">Forgot password?</a>
+                            </div>
+    
+                            <input type="button" className="btn btn-primary" value="Login" onClick={this.onLoginClick} />
+    
+                        </form>
+                        
+                        </div>
+    
+                        <p className="botto-text"> Designed by Putra</p>
+                    </div>
+                </div>
+                </div>
+                    );
+                }
+        
 
                 return <Redirect to="/" />
             }
